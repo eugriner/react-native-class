05bbf92ebc81ceeea04143b79f110297654c3018
Curso de Verão <cursodeverao@A103-05L.imd.local>
2019-01-19 11:50:05 -0300

    Componetization

--- MyApp/App.js
+++ MyApp/App.js
@@ -16,12 +16,20 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+class Hello extends Component {
+  render() {
+    return (
+      <Text style={styles.welcome}>Welcome to this React Native!</Text>
+    )
+  }
+}
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Hello />
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
